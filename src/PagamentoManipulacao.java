420a64e84b8754822d78d5a8553d1a7728713a18
Alison8547 <alisonailson100@hotmail.com>
2022-10-08 02:12:11 -0300

    pagamento manipulacao

---
+++ src/PagamentoManipulacao.java
@@ -0,0 +1,12 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class PagamentoManipulacao extends Crud{
+
+    private List<Pagamento> pagamentoList = new ArrayList<>();
+
+    public void updateList(Integer index,Pagamento pagamento) {
+        Pagamento pagamentoBuscado = pagamentoList.get(index);
+        // coisas a pensar ainda
+    }
+}
