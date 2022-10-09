9ecf379443299290fd2e15d4b8445ffe91dd49e1
Kevin Aryel <kevinaryel.dev@gmail.com>
2022-10-09 15:28:36 -0300

    fix: Arrumando a função de editar voos

--- src/VooManipulacao.java
+++ src/VooManipulacao.java
@@ -1,5 +1,6 @@
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 public class VooManipulacao extends Crud{
@@ -9,13 +10,19 @@ public class VooManipulacao extends Crud{
     }
     private List<Voo> listVoos;
 
-    public void updateList(Integer index, Voo obj) {
-        Voo vooBuscado = getListVoos().get(index);
-//        vooBuscado.getPassageiros(null);
-        vooBuscado.setDataPartida(obj.getDataPartida());
-        vooBuscado.setDataChegada(obj.getDataChegada());
-        vooBuscado.setLocalPartida(obj.getLocalPartida());
-        vooBuscado.setLocalChegada(obj.getLocalChegada());
-        vooBuscado.setPrecoPassagem(obj.getPrecoPassagem());
+    public void updateList(Integer id, Voo obj) {
+        Optional<Voo> vooBuscado = getListVoos().stream()
+                .filter(voo -> voo.getId() == id ).findFirst();
+        if (vooBuscado.isPresent()){
+           Voo vooAchado = vooBuscado.get();
+            vooAchado.setDataPartida(obj.getDataPartida());
+            vooAchado.setDataChegada(obj.getDataChegada());
+            vooAchado.setLocalPartida(obj.getLocalPartida());
+            vooAchado.setLocalChegada(obj.getLocalChegada());
+            vooAchado.setPrecoPassagem(obj.getPrecoPassagem());
+            System.out.println("Voo editado com sucesso");
+        } else {
+            System.out.println("Id errado!");
+        }
     }
 }
