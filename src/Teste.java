7f50311ea6ceed0bc7ebdd8752b7f65360d9e211
pietroking <pietrobking@gmail.com>
2022-10-09 16:23:36 -0300

    testes unitarios

--- src/Teste.java
+++ src/Teste.java
@@ -12,73 +12,15 @@ public class Teste {
     CompanhiaManipulacao companhiaManipulacao;
     SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
     Cliente cliente;
-    Cliente cliente1 = new Cliente("Pietro",26,"00566783045","pietro.bianchi@dbccompany.br","994668885");
-    Cliente cliente2 = new Cliente("Alison",24,"9999999999","alison.silva@dbccompany.br","6666666666");
-    Cliente cliente3 = new Cliente("Kevin",25,"88888888888","kevin.aryel@dbccompany.br","77777777777");
-    Cliente cliente4 = new Cliente("Maicon",31,"11111111111","maicon.gerardi@dbccompany.br","5555555555");
-    Cliente cliente5 = new Cliente("Ricardo",27,"2222222222","ricador.langbecker@dbccompany.br","4444444444");
-    Cliente cliente6 = new Cliente("Gaby",24,"3333333333","gaby.freitas@dbccompany.br","33333333333");
-    Cliente cliente7 = new Cliente("Felipe",26,"4444444444","felipe.noguez@dbccompany.br","888888888");
-    Cliente cliente8 = new Cliente("Lucas",26,"0000000000","lucas.pereira@dbccompany.br","000000000");
-    //        clienteManipulacao.createList(cliente1);
-//        clienteManipulacao.createList(cliente2);
-//        clienteManipulacao.createList(cliente3);
-//        clienteManipulacao.createList(cliente4);
-//        clienteManipulacao.createList(cliente5);
-//        clienteManipulacao.createList(cliente6);
-//        clienteManipulacao.createList(cliente7);
-//        clienteManipulacao.createList(cliente8);
-    CompanhiaAerea companhiaAerea1 = new CompanhiaAerea("Gol");
-    CompanhiaAerea companhiaAerea2 = new CompanhiaAerea("Tam");
-    CompanhiaAerea companhiaAerea3 = new CompanhiaAerea("Azul");
-    CompanhiaAerea companhiaAerea4 = new CompanhiaAerea("Emirates Airlines");
-    //        companhiaManipulacao.createList(companhiaAerea1);
-//        companhiaManipulacao.createList(companhiaAerea2);
-//        companhiaManipulacao.createList(companhiaAerea3);
-//        companhiaManipulacao.createList(companhiaAerea4);
-    Voo voo1 = new Voo("Gol",(sdf.parse("10/10/2022")),(sdf.parse("12/10/2022")),"Porto Alegre/RS/BR","Montivideo/UY", 350.50);
-    Voo voo2 = new Voo("Emirates Airlines",sdf.parse("17/10/2022"),sdf.parse("22/10/2022"),"Porto Alegre/RS/BR","Dubai/EM", 33350.50);
-    Voo voo3 = new Voo("Tam",sdf.parse("15/10/2022"),sdf.parse("15/10/2022"),"Porto Alegre/RS/BR","Rio de Janeiro/RJ/BR", 250.90);
-    Voo voo4 = new Voo("Azul",(sdf.parse("23/12/2022")),sdf.parse("27/12/2022"),"Porto Alegre/RS/BR","Natal/RN/BR", 490.68);
-    Voo voo5 = new Voo("Azul",sdf.parse("11/11/2022"),sdf.parse("12/11/2022"),"Porto Alegre/RS/BR","São Paulo/SP/BR", 325.37);
-    Voo voo6 = new Voo("Emirates Airlines",sdf.parse("13/12/2022"),sdf.parse("17/12/2022"),"Dubai/EM","Porto Alegre/RS/BR", 27350.50);
-    Voo voo7 = new Voo("Tam",sdf.parse("11/11/2022"),sdf.parse("12/11/2022"),"Porto Alegre/RS/BR","São Paulo/SP/BR", 400.37);
-    Voo voo8 = new Voo("Gol",sdf.parse("11/11/2022"),sdf.parse("12/11/2022"),"Porto Alegre/RS/BR","São Paulo/SP/BR", 368.37);
+    CompanhiaAerea companhiaAerea;
+    Voo voo;
 
-    public Teste() throws ParseException {
-    }
-//        vooManipulacao.createList(voo1);
-//        vooManipulacao.createList(voo2);
-//        vooManipulacao.createList(voo3);
-//        vooManipulacao.createList(voo4);
-//        vooManipulacao.createList(voo5);
-//        vooManipulacao.createList(voo6);
-//        vooManipulacao.createList(voo7);
-//        vooManipulacao.createList(voo8);
 
     @BeforeEach
     public void setUp() throws ParseException {
         clienteManipulacao = new ClienteManipulacao();
         vooManipulacao = new VooManipulacao();
         companhiaManipulacao = new CompanhiaManipulacao();
-        voo1.addPassageiros(cliente1);
-        voo1.addPassageiros(cliente3);
-        voo2.addPassageiros(cliente8);
-        voo2.addPassageiros(cliente5);
-        voo2.addPassageiros(cliente6);
-        voo2.addPassageiros(cliente2);
-        voo3.addPassageiros(cliente1);
-        voo3.addPassageiros(cliente5);
-        voo3.addPassageiros(cliente4);
-        voo3.addPassageiros(cliente7);
-        voo5.addPassageiros(cliente8);
-        voo5.addPassageiros(cliente5);
-        voo5.addPassageiros(cliente6);
-        voo5.addPassageiros(cliente2);
-        voo6.addPassageiros(cliente8);
-        voo6.addPassageiros(cliente5);
-        voo6.addPassageiros(cliente6);
-        voo6.addPassageiros(cliente2);
     }
 
 
@@ -92,13 +34,14 @@ public class Teste {
     }
 
     @Test
-    public void deveTestarMetodoDeletarCliente() {
-        cliente = new Cliente("Alison", 22, "444-444-333-44", "alison@hotmail.com", "9999-9999");
-        clienteManipulacao.createList(cliente);
+    public void deveTestarMetedoReadCliente(){
+        Cliente cliente1 = new Cliente("Pietro",26,"00566783045","pietro.bianchi@dbccompany.br","994668885");
+        Cliente cliente2 = new Cliente("Alison",24,"9999999999","alison.silva@dbccompany.br","6666666666");
 
-        clienteManipulacao.deleteList(0);
+        clienteManipulacao.createList(cliente1);
+        clienteManipulacao.createList(cliente2);
 
-        Assertions.assertNull(clienteManipulacao.listar());
+        Assertions.assertEquals(clienteManipulacao.readList(1), cliente2);
     }
 
     @Test
@@ -119,5 +62,99 @@ public class Teste {
         Assertions.assertEquals(cliente2.getTelefone(), cliente.getTelefone());
     }
 
+    @Test
+    public void deveTestarMetodoDeleteCliente() {
+        Cliente cliente1 = new Cliente("Pietro",26,"00566783045","pietro.bianchi@dbccompany.br","994668885");
+        Cliente cliente2 = new Cliente("Alison",24,"9999999999","alison.silva@dbccompany.br","6666666666");
+        Cliente cliente3 = new Cliente("Kevin",25,"88888888888","kevin.aryel@dbccompany.br","77777777777");
+        clienteManipulacao.createList(cliente1);
+        clienteManipulacao.createList(cliente2);
+        clienteManipulacao.createList(cliente3);
 
+        clienteManipulacao.deleteList(0);
+
+        Assertions.assertEquals(2, clienteManipulacao.getList().size());
+    }
+
+    @Test
+    public void deveTestarMetedoCreateVoo() throws ParseException {
+        voo = new Voo("Gol",(sdf.parse("10/10/2022")),(sdf.parse("12/10/2022")),"Porto Alegre/RS/BR","Montivideo/UY", 350.50);
+
+        vooManipulacao.createList(voo);
+
+        Assertions.assertNotEquals(Collections.EMPTY_LIST, vooManipulacao);
+    }
+    @Test
+    public void deveTestarMetedoReadVoo() throws ParseException {
+        Voo voo1 = new Voo("Gol",(sdf.parse("10/10/2022")),(sdf.parse("12/10/2022")),"Porto Alegre/RS/BR","Montivideo/UY", 350.50);
+        Voo voo2 = new Voo("Emirates Airlines",sdf.parse("17/10/2022"),sdf.parse("22/10/2022"),"Porto Alegre/RS/BR","Dubai/EM", 33350.50);
+
+        vooManipulacao.createList(voo1);
+        vooManipulacao.createList(voo2);
+
+        Assertions.assertEquals(vooManipulacao.readList(1), voo2);
+    }
+//    @Test
+//    public void deveTestarMetedoUpdateVoo() throws ParseException {
+//        Voo voo1 = new Voo("Gol",(sdf.parse("10/10/2022")),(sdf.parse("12/10/2022")),"Porto Alegre/RS/BR","Montivideo/UY", 350.50);
+//        vooManipulacao.createList(voo1);
+//        Voo voo2 = new Voo();
+//        voo2.setDataPartida(sdf.parse("17/10/2022"));
+//        voo2.setDataChegada(sdf.parse("22/10/2022"));
+//        voo2.setLocalPartida("Porto Alegre/RS/BR");
+//        voo2.setLocalChegada("Dubai/EM");
+//        voo2.setPrecoPassagem(33350.50);
+//        vooManipulacao.updateList(0, voo2);
+//
+//        Assertions.assertEquals(voo2.getDataPartida(), voo1.getDataPartida());
+//        Assertions.assertEquals(voo2.getDataChegada(), voo1.getDataChegada());
+//        Assertions.assertEquals(voo2.getLocalPartida(), voo1.getLocalPartida());
+//        Assertions.assertEquals(voo2.getLocalChegada(), voo1.getLocalChegada());
+//        Assertions.assertEquals(voo2.getPrecoPassagem(), voo1.getPrecoPassagem());
+//
+//    }
+    @Test
+    public void deveTestarMetedoDeleteVoo() throws ParseException {
+        Voo voo1 = new Voo("Gol",(sdf.parse("10/10/2022")),(sdf.parse("12/10/2022")),"Porto Alegre/RS/BR","Montivideo/UY", 350.50);
+        vooManipulacao.createList(voo1);
+        vooManipulacao.deleteList(0);
+
+        Assertions.assertEquals(0, vooManipulacao.getList().size());
+    }
+    @Test
+    public void deveTestarMetedoCreateCompanhia(){
+        companhiaAerea = new CompanhiaAerea("Gol");
+        companhiaManipulacao.createList(companhiaAerea);
+
+        Assertions.assertNotEquals(Collections.EMPTY_LIST, companhiaAerea);
+    }
+    @Test
+    public void deveTestarMetedoReadCompanhia(){
+        CompanhiaAerea companhiaAerea2 = new CompanhiaAerea("Tam");
+        CompanhiaAerea companhiaAerea3 = new CompanhiaAerea("Azul");
+
+        companhiaManipulacao.createList(companhiaAerea2);
+        companhiaManipulacao.createList(companhiaAerea3);
+
+        Assertions.assertEquals(companhiaManipulacao.readList(0), companhiaAerea2);
+    }
+    @Test
+    public void deveTestarMetedoUpdateCompanhia(){
+        CompanhiaAerea companhiaAerea1 = new CompanhiaAerea("Gol");
+        companhiaManipulacao.createList(companhiaAerea1);
+        CompanhiaAerea companhiaAerea4 = new CompanhiaAerea("Emirates Airlines");
+        companhiaManipulacao.updateList(0,companhiaAerea4);
+
+        Assertions.assertEquals(companhiaAerea4.getNome(), companhiaAerea1.getNome());
+    }
+    @Test
+    public void deveTestarMetedoDeleteCompanhia(){
+        CompanhiaAerea companhiaAerea1 = new CompanhiaAerea("Gol");
+        CompanhiaAerea companhiaAerea4 = new CompanhiaAerea("Emirates Airlines");
+        companhiaManipulacao.createList(companhiaAerea1);
+        companhiaManipulacao.createList(companhiaAerea4);
+        companhiaManipulacao.deleteList(0);
+
+        Assertions.assertEquals(1, companhiaManipulacao.getList().size());
+    }
 }
